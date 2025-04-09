891da95e01ea2601e78849ca68649d3aa414113b
Chris Tang <1073412@hr.nl>
2025-04-09 17:40:32 +0200

    Eerste commit

---
+++ model-training-NN/script.js
@@ -0,0 +1,44 @@
+import posedata from './testdata.json' with {type: 'json'};
+
+const statusDiv = document.getElementById("status");
+
+let nn; //Neural Network object
+
+function startTraining(){
+    statusDiv.textContent = "Training Neural Network...";
+    ml5.setBackend("webgl");
+
+    nn = ml5.neuralNetwork({
+        task: 'classification',
+        debug: true,
+        // learningRate: 0.01
+    });
+
+    console.log('adding ${posedata.length} poses');
+
+    for(let pose of posedata){
+        // console.log(pose);
+        nn.addData(pose.data, {label:pose.label});
+    }
+
+    nn.normalizeData();
+    nn.train({epochs: 30}, finishedTraining);
+
+}
+
+
+function finishedTraining(){
+    statusDiv.textContent = "Training Complete!";
+    console.log("Training Complete");
+    nn.save(); // Save the model
+
+    let demopose = posedata[10].data;
+    nn.classify(demopose, (result) => {
+        console.log(`I think this pose is a ${result[0].label}`);
+        console.log(`I am ${result[0].confidence.toFixed(2)* 100} % sure`);
+
+    });
+
+}
+
+startTraining();
